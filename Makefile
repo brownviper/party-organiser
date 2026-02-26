561c91330d43be6ed377e0cb3e45f6e8585c5c6e
brownviper <10910486+brownviper@users.noreply.github.com>
2026-02-26 14:00:31 +0000

    seeding database with test data (#2)

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+start-env:
+	uv venv --python 3.13.0 --clear
+	uv sync
+	echo "run the following command '''source .venv/bin/activate'''"
+
 migrate:
 	alembic upgrade head
+
+fill-test-data:
+	export PYTHONPATH="$$PWD:$$PYTHONPATH"; \
+	echo "Building in $$PYTHONPATH";
+	python party_app/initial_data/load_initial_data_to_db.py
--- party_app/dependency.py
+++ party_app/dependency.py
@@ -1,8 +1,9 @@
 import os
 from typing import Annotated
-
+from sqlmodel import Session
 from fastapi import Depends
 from fastapi.templating import Jinja2Templates
+from db import engine
 
 
 _templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
@@ -13,3 +14,7 @@ def _get_templates():
 
 
 Templates = Annotated[Jinja2Templates, Depends(_get_templates)]
+
+def get_session():
+    with Session(engine) as session:
+        yield session
